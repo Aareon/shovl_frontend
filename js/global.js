ead7409289744c10de829f66be4d249872876978
Digital Iceberg <zachechouafni@gmail.com>
2016-09-03 14:03:32 +0100

    Forgot to uncomment auth code

--- js/global.js
+++ js/global.js
@@ -1,4 +1,4 @@
-/*function isloggedin(){
+function isloggedin(){
 	if(localStorage.getItem("token") === null){
 			window.location.assign("/app/login");
 		}
@@ -21,7 +21,7 @@
             async: false
 			});	
 	}	
-}*/
+}
 
 function logout(){
 	 $.ajax({
