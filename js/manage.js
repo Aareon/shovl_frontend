d4ed314f329eea74a452f629a0114d97f2b3f82c
Digital Iceberg <zachechouafni@gmail.com>
2016-12-01 01:24:41 +0000

    Manage: Move bug

--- js/manage.js
+++ js/manage.js
@@ -208,7 +208,7 @@ $('#fm-move').click(function() {
 			}else{
 			  sendir = currentdir+$(this).val();
 			}
-          FM_Move(senddir, senddir);
+          FM_Move(currentdir+$(this).val(), senddir);
         });
         pagealert("success", "Moved selected elements.");
         FM_DisplayCurrentDir(currentdir);
