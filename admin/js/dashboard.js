a06e2ab41da47599a9948041720c1016992c0555
Digital Iceberg <zachechouafni@gmail.com>
2016-12-08 19:53:09 +0000

    Update system for admin

---
+++ admin/js/dashboard.js
@@ -0,0 +1,17 @@
+function UpdateBackend(){
+		   isloggedin();
+            $.ajax({
+            type: "GET",
+            url: "/api/admin/update",
+            beforeSend: function (request)
+            {
+                request.setRequestHeader("Authorization", localStorage.getItem("token"));
+            },
+            success: function(result) {
+				          pagealert("success", result);
+            },
+            error: function(result) {
+                  pagealert("error", result.responseText);
+            },
+    });
+}
