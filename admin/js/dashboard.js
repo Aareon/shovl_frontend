89f5ea3b4b4b5f18e3d49862d54ee4052f21b8f5
Digital Iceberg <zachechouafni@gmail.com>
2016-12-10 19:08:18 +0000

    Admin: CDN update system

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -15,3 +15,21 @@ function UpdateBackend(){
             },
     });
 }
+
+function UpdateCDN(){
+		   isloggedin();
+            $.ajax({
+            type: "GET",
+            url: "/api/admin/cdnupdate",
+            beforeSend: function (request)
+            {
+                request.setRequestHeader("Authorization", localStorage.getItem("token"));
+            },
+            success: function(result) {
+				          pagealert("success", result);
+            },
+            error: function(result) {
+                  pagealert("error", result.responseText);
+            },
+    });
+}
