db26ab35e2c9d3fb24252a935cc0861112d6d457
Digital Iceberg <zachechouafni@gmail.com>
2016-12-15 13:11:00 +0000

    Event for slider

--- js/shieldmanage.js
+++ js/shieldmanage.js
@@ -187,8 +187,8 @@ $('#auto-renew').click(function() {
    });
 });
 
-$('#waf-box').click(function() {
-	var req = {hostname: $_GET("id"), enabled: !$(this).is(':checked')};
+$('#waf-box').change(function() {
+	var req = {hostname: $_GET("id"), enabled: !$(this).prop(('checked')};
         $.ajax({
            type:"POST",
            url: "/api/shield/settings/waf",
@@ -206,8 +206,8 @@ $('#waf-box').click(function() {
    });
 });
 
-$('#cache-box').click(function() {
-	var req = {hostname: $_GET("id"), enabled: $(this).is(':checked')};
+$('#cache-box').change(function() {
+	var req = {hostname: $_GET("id"), enabled: $(this).prop(('checked')};
         $.ajax({
            type:"POST",
            url: "/api/shield/settings/cache",
@@ -225,8 +225,8 @@ $('#cache-box').click(function() {
    });
 });
 
-$('#forcehttps-box').click(function() {
-	var req = {hostname: $_GET("id"), enabled: $(this).is(':checked')};
+$('#forcehttps-box').change(function() {
+	var req = {hostname: $_GET("id"), enabled: $(this).prop(('checked')};
         $.ajax({
            type:"POST",
            url: "/api/shield/settings/https",
@@ -244,7 +244,7 @@ $('#forcehttps-box').click(function() {
    });
 });
 
-$("#servuce_upgrade").click(function(){
+$("#service_upgrade").click(function(){
 	var req = {hostname: $_GET("id"), servceid: parseInt(document.querySelector('input[name="SID"]:checked').value)};
 	if (document.querySelector('input[name="SID"]:checked') == null){
 		pagealert("error", "You forgot to select a package");
@@ -284,8 +284,8 @@ function getservices(){
           if (data.name != currentservice) {
 					tr = $("<input class='deploy_checkbox' name='SID' value='" + data[i].name + "' id='SID"+data[i].name+"' type='radio'></input>");
 					lbl = $("<label for='SID" + data[i].name + "'> <span class='deploy_checkbox_icon'><i class='fa fa-money package_icon' style='font-size: 1em;'></i></span><span class='deploy_checkbox_line1'>" + data[i].name + ": $"+data[i].price+"/Month</span></span></label>");
-					$('#services').append(tr);
-					$('#services').append(lbl);
+					$('#services_list').append(tr);
+					$('#services_list').append(lbl);
           }
 				}
 
