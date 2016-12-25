e97e54d44684dd744f4c2c7b21ed0d4267c766eb
Digital Iceberg <zachechouafni@gmail.com>
2016-12-25 20:06:55 +0000

    Stats for admin

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -1,36 +1,32 @@
 $(document).ready(function(){
 	setInterval(function(){
 		getstats();
+		getmetrics();
 	}, 1000);
 });
 
 function getmetrics(){
+	isloggedin();
+				$.ajax({
+					 type:"GET",
+					 url: "/api/admin/stats",
+					 data: JSON.stringify(hostname),
+					 beforeSend: function (request)
+					 {
+							 request.setRequestHeader("Authorization", localStorage.getItem("token"));
+					 },
+					 success: function(result) {
+						 var data = JSON.parse(result);
+						 $("#cpu").html("<span>"+data.cpu+"</span>");
+						 $("#cpu").css("width", data.cpu);
 
+						 $("#memory").html("<span>"+data.memory+" "+data.memoryused+"MB/"+data.memorytotal+"MB"+"</span>");
+						 $("#memory").css("width", data.memory);
 
-}
-
-function getstats(){
-	 isloggedin();
-         $.ajax({
-            type:"GET",
-            url: "/api/admin/stats",
-            data: JSON.stringify(hostname),
-            beforeSend: function (request)
-            {
-                request.setRequestHeader("Authorization", localStorage.getItem("token"));
-            },
-            success: function(result) {
-							var data = JSON.parse(result);
-							$("#cpu").html("<span>"+data.cpu+"</span>");
-							$("#cpu").css("width", data.cpu);
-
-							$("#memory").html("<span>"+data.memory+" "+data.memoryused+"MB/"+data.memorytotal+"MB"+"</span>");
-							$("#memory").css("width", data.memory);
-
-							$("#disk").html("<span>"+data.disk+" "+data.diskused+"GB/"+data.disktotal+"GB"+"</span>");
-							$("#disk").css("width", data.disk);
-          },
-    });
+						 $("#disk").html("<span>"+data.disk+" "+data.diskused+"GB/"+data.disktotal+"GB"+"</span>");
+						 $("#disk").css("width", data.disk);
+				 },
+	 });
 }
 
 function UpdateBackend(){
