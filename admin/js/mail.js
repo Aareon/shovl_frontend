6e9a7f3ed5a254cd50c43ae37d7284da02060424
Digital Iceberg <zachechouafni@gmail.com>
2017-01-01 01:55:37 +0000

    Mail sending admin

---
+++ admin/js/mail.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	isloggedin();
+});
+
+
+$("#send-email").click(function(){
+     var mailreq = {prefix: $("#prefix").val(), recipient: $("#recipient").val(), subject: $("#subject").val(), body: $("#body").val("");};
+		 isloggedin();
+         $.ajax({
+            type:"POST",
+            url: "/api/admin/sendmail",
+            data: JSON.stringify(mailreq),
+            beforeSend: function (request)
+            {
+                request.setRequestHeader("Authorization", localStorage.getItem("token"));
+            },
+            success: function(result) {
+                pagealert("success", result);
+								$("#prefix").val("");
+								$("#recipient").val("");
+								$("#subject").val("");
+								$("#body").val("");
+            },
+            error: function(result) {
+							pagealert("error", result.responseText);
+				  }
+	});
+});
