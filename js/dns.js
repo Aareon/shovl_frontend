52fba85aecda2e74331876106293bf0eafcf87b4
Digital Iceberg <zachechouafni@gmail.com>
2017-01-19 19:00:28 +0000

    secure backend support

--- js/dns.js
+++ js/dns.js
@@ -8,7 +8,7 @@ $("#record-create").click(function(){
   if (type == "MX"){
       req = {hostname: $_GET("id"), name: name+$_GET("id"), protected: $("#shield-box").prop(('checked')), type: type, record_mx_server: $("#record-mx-server").val(), record_mx_priority:  parseInt($("#record-mx-priority").val())};
   }else if (type == "A"){
-      req = {hostname: $_GET("id"), name: name+$_GET("id"), protected: $("#shield-box").prop(('checked')), type: type, record_a: $("#record-a").val()};
+      req = {securebackend: $("#securebackend-box").prop(('checked')), hostname: $_GET("id"), name: name+$_GET("id"), protected: $("#shield-box").prop(('checked')), type: type, record_a: $("#record-a").val()};
   }else if (type == "AAAA"){
       req = {hostname: $_GET("id"), name: name+$_GET("id"), protected: $("#shield-box").prop(('checked')), type: type, record_aaaa: $("#record-aaaa").val()};
   }else if (type == "TXT"){
@@ -16,7 +16,7 @@ $("#record-create").click(function(){
   }else if (type == "SPF"){
       req = {hostname: $_GET("id"), name: name+$_GET("id"), protected: $("#shield-box").prop(('checked')), type: type, record_spf: $("#record-spf").val()};
   }else if (type == "CNAME"){
-      req = {hostname: $_GET("id"), name: name+$_GET("id"), protected: $("#shield-box").prop(('checked')), type: type, record_cname: $("#record-cname").val()};
+      req = {securebackend: $("#securebackend-box").prop(('checked')), hostname: $_GET("id"), name: name+$_GET("id"), protected: $("#shield-box").prop(('checked')), type: type, record_cname: $("#record-cname").val()};
   }else if (type == "SRV"){
       req = {hostname: $_GET("id"), name: name+$_GET("id"), protected: $("#shield-box").prop(('checked')), type: type, record_srv_target: $("#record-srv-target").val(), record_srv_service: $("#record-srv-service").val(), record_srv_protocol: $("#record-srv-protocol").val(), record_srv_port:  parseInt($("#record-srv-port").val()), record_srv_weight: parseInt($("#record-srv-weight").val()), record_srv_priority: parseInt($("#record-srv-priority").val())};
   }
@@ -45,6 +45,7 @@ $("#record-type").change(function(){
     $("#val-mx").fadeIn(800);
   }else if (type == "A"){
       $("#val-a").fadeIn(800);
+      $("#securebackend-div").fadeIn(800);
   }else if (type == "AAAA"){
       $("#val-aaaa").fadeIn(800);
   }else if (type == "TXT"){
@@ -53,6 +54,7 @@ $("#record-type").change(function(){
       $("#val-spf").fadeIn(800);
   }else if (type == "CNAME"){
       $("#val-cname").fadeIn(800);
+      $("#securebackend-div").fadeIn(800);
   }else if (type == "SRV"){
       $("#val-srv").fadeIn(800);
   }
@@ -66,4 +68,5 @@ function HideDNSForms(type){
   $("#val-txt").hide();
   $("#val-srv").hide();
   $("#val-mx").hide();
+  $("#securebackend-div").hide();
 }
